bb123aff21b065cda09693de1e8cda07f9a20d48
EpicEmeraldPlayz <smrmakeyt@gmail.com>
2021-11-09 10:33:29 +0700

    Init Commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,8 @@ $(function() {
 
     const body = $('body');
 
-    body.attr('data-scroll-direction', 'down');
+    body.attr('data-scroll-direction', 'up');
+    body.attr('data-loading-assets', 'true');
 
     let lastScrollTop = 0;
 
@@ -23,4 +24,23 @@ $(function() {
         }
     }, false);
 
+    // Credits: https://stackoverflow.com/a/60949881/15760608
+    Promise.all(Array.from(document.images).map(img => {
+        if (img.complete)
+            if (img.naturalHeight !== 0)
+                return Promise.resolve();
+            else
+                return Promise.reject(img);
+        return new Promise((resolve, reject) => {
+            img.addEventListener('load', resolve);
+            img.addEventListener('error', () => reject(img));
+        });
+    })).then(() => {
+        body.attr('data-loading-assets', 'false');
+        $('body > .preloader').fadeOut(1000);
+        $('body > section').hide().fadeIn(1000);
+    }, failed => {
+        console.log('Some image failed to load, others may still be loading');
+        console.log(failed);
+    });
 });
